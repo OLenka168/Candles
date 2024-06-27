9d7a20854b03b4df587740c50b8889afd336fc40
OLenka168 <120338125+OLenka168@users.noreply.github.com>
2024-06-27 20:08:12 +0300

    Update CandleModel.js

--- CandleModel.js
+++ CandleModel.js
@@ -4,6 +4,14 @@ const candleSchema = new mongoose.Schema({
     img: {
         type: String,
         required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
     }
 }) 
 
